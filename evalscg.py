764f3ffcc55161948f0c17981277b40680450488
wsonguga <wsong@sensorweb.hsd1.ga.comcast.net>
2021-03-08 21:59:57 -0500

    add k-shape

--- evalscg.py
+++ evalscg.py
@@ -14,8 +14,11 @@ import matplotlib.pyplot as plt
 import random
 from scipy import signal
 #%matplotlib inline
+from tslearn.clustering import KShape
+from tslearn.datasets import CachedDatasets
+from tslearn.preprocessing import TimeSeriesScalerMeanVariance
 
-plt.rcParams['figure.figsize'] = [20, 4]  # Bigger images
+plt.rcParams['figure.figsize'] = [20, 8]  # Bigger images
 
 good_data = np.load("./data/scg/good_data.npy")
 bad_data = np.load("./data/scg/bad_data.npy")
@@ -44,8 +47,6 @@ from sklearn.metrics.pairwise import pairwise_distances
 
 all_data = arr = np.concatenate((good_data, bad_data), axis=0)
 
-print(all_data.shape)
-
 N = 20
 for ind in range(N):
     # plt.figure(figsize=(20, 4))
@@ -63,6 +64,10 @@ for ind in range(N):
 
 plt.show()
 
+distance = np.corrcoef(all_data)
+plt.imshow(distance)
+plt.show()
+print(distance)
 
 # distance = pairwise_distances(all_data, all_data, metric='manhattan')
 # print("manhattan:", distance)
@@ -70,13 +75,44 @@ plt.show()
 # plt.imshow(distance)
 # plt.show()
 
-distance = pairwise_distances(all_data, all_data, metric='cosine')
-# print("cosine:", distance)
-plt.imshow(distance)
+# distance = pairwise_distances(all_data, all_data, metric='cosine')
+# # print("cosine:", distance)
+# plt.imshow(distance)
+# plt.show()
+
+# In[2]
+np.random.shuffle(all_data)
+print(all_data.shape)
+
+# For this method to operate properly, prior scaling is required
+x_train = TimeSeriesScalerMeanVariance().fit_transform(all_data)
+sz = x_train.shape[1]
+
+# kShape clustering
+seed = 0
+ks = KShape(n_clusters=2, verbose=True, random_state=seed)
+y_pred = ks.fit_predict(x_train)
+
+print(x_train.shape)
+print(y_pred.shape)
+
+plt.figure()
+for yi in range(2):
+    N = len(x_train[y_pred == yi])
+    ind = 0
+    for xx in x_train[y_pred == yi]:
+        plt.subplot(N, 2, 2*ind+yi+1)
+        plt.plot(xx)
+        ind += 1
+        # plt.plot(xx.ravel(), "k-", alpha=.2)
+    # plt.plot(ks.cluster_centers_[yi].ravel(), "r-")
+    # plt.xlim(0, sz)
+    # plt.ylim(-4, 4)
+    plt.title("Cluster %d, N %d" % (yi + 1, N))
+    plt.tight_layout()
 plt.show()
 
 
-distance = np.corrcoef(all_data)
-plt.imshow(distance)
-plt.show()
 
+
+# %%
