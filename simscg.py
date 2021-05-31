54aac99a19862c0d60840197e8e6f3da10d5c0cb
Paratra <ming.song.cn@outlook.com>
2021-05-31 14:17:28 -0400

    update

--- simscg.py
+++ simscg.py
@@ -17,66 +17,70 @@ from scipy import signal
 
 plt.rcParams['figure.figsize'] = [20, 10]  # Bigger images
 
-N = 10
-duration = 10
+gen_list = [
+            ['training_set',5000, 0.15], #5000
+            ['good_set',500, 0.15], #1500
+            ['bad_set',500, 0.75], #1500
+            ['mixed_set',500, -1],
+            ]
 
-for ind in range(N):
-    heart_rate = random.randint(60, 90)
-    respiration_rate = random.randint(10, 20)
-    systolic = random.randint(90,150)
-    diastolic = random.randint(70,100)
-    fs = 100
+for gen_info in gen_list:
+    # import pdb; pdb.set_trace()
+    file_name = gen_info[0]
+    N = int(gen_info[1])
 
-    data = nk.scg_simulate(duration=duration, sampling_rate=fs, noise=0.15, heart_rate=heart_rate, systolic=systolic, diastolic=diastolic)
-    x = data #/np.linalg.norm(data)
-
-    fmin = 0
-    fmax = 10
-    npseg = 200
-    slidingstep=20
+    duration = 10
+    simulated_data = []
 
     # plt.figure(figsize=(20, 4))
-    # plt.subplot(N,2,2*ind+1)
-    plt.subplot(N,1,ind+1)
 
-    plt.plot(x)
-    plt.title(f'Raw, HR {heart_rate} SBP {systolic} DBP {diastolic}')
-    plt.ylabel('Amplitude')
-    plt.xlabel('Number of Samples')
+    for ind in range(N):
+        heart_rate = random.randint(60, 90)
+        systolic = random.randint(90,150)
+        diastolic = random.randint(70,100)
+        fs = 100
 
+        noise = float(gen_info[2])
+        if noise == -1:
+            noise = random.choice([0.15, 0.5, 0.75])
+            # print (noise) 
 
+        data = nk.scg_simulate(duration=duration, sampling_rate=fs, noise=noise, heart_rate=heart_rate, systolic=systolic, diastolic=diastolic)
+        simulated_data.append(list(data)+[heart_rate]+[systolic]+[diastolic])
 
-    # f, t, Zxx = signal.stft(x, fs, nperseg=npseg, noverlap = npseg-slidingstep)
-    # f, t, Zxx = signal.spectrogram(x, fs)
+        # plot the signals and spectrogram
+        x = data #/np.linalg.norm(data)
+        fmin = 0
+        fmax = 10
+        npseg = 200
+        slidingstep=20
 
-    # plt.subplot(N,2,2*ind+2)
-    # plt.pcolormesh(t, f[int(fmin*npseg/fs):int(fmax*npseg/fs)+1], np.abs(Zxx[int(fmin*npseg/fs):int(fmax*npseg/fs)+1,:]), shading='gouraud')
-    # #plt.pcolormesh(t, f, np.abs(Zxx), shading='gouraud')
-    # plt.title(f'STFT, HR {heart_rate} SBP {systolic} DBP {diastolic}')
-    # plt.ylabel('Frequency [Hz]')
-    # plt.xlabel('Time [sec]')
-    ind +=1
-
-plt.show()
-
-# # Alternate heart rate and noise levels
-# scg100 = nk.scg_simulate(duration=10, noise=0.05, heart_rate=60, systolic=100, method="simple")
-# scg120 = nk.scg_simulate(duration=10, noise=0.05, heart_rate=60, systolic=120, method="simple")
-# scg140 = nk.scg_simulate(duration=10, noise=0.05, heart_rate=60, systolic=140, method="simple")
-
-# # scg100 = nk.scg_simulate(duration=10, noise=0.05, heart_rate=60, method="scgsyn")
-
-# # Visualize
-# # scg_df = pd.DataFrame({"SCG_100": scg100,
-# #                        "SCG_50": scg50})
-# scg_df = pd.DataFrame({"SCG_100": scg100,
-#                        "SCG_120": scg120,
-#                        "SCG_140": scg140})
+        # import pdb; pdb.set_trace()
 
 
-# nk.signal_plot(scg_df, subplots=True)
-# plt.show()
+        # plt.subplot(N,2,2*ind+1)
 
-# In[3]
+        # plt.subplot(N,1,ind+1)
+        
+        # plt.plot(x)
+        # plt.title(f'Raw, HR {heart_rate} SBP {systolic} DBP {diastolic} Noise {noise}')
+        # plt.ylabel('Amplitude')
+        # plt.xlabel('Number of Samples')
 
-# %%
+        # plot spectrogram
+        # f, t, Zxx = signal.stft(x, fs, nperseg=npseg, noverlap = npseg-slidingstep)
+        # f, t, Zxx = signal.spectrogram(x, fs)
+
+        # plt.subplot(N,2,2*ind+2)
+        # plt.pcolormesh(t, f[int(fmin*npseg/fs):int(fmax*npseg/fs)+1], np.abs(Zxx[int(fmin*npseg/fs):int(fmax*npseg/fs)+1,:]), shading='gouraud')
+        # #plt.pcolormesh(t, f, np.abs(Zxx), shading='gouraud')
+        # plt.title(f'STFT, HR {heart_rate} SBP {systolic} DBP {diastolic}')
+        # plt.ylabel('Frequency [Hz]')
+        # plt.xlabel('Time [sec]')
+        ind +=1
+
+    # plt.show()
+    simulated_data = np.asarray(simulated_data)
+    np.save(f'./data/{file_name}',simulated_data)
+    print(f'{file_name} is generated and saved!')
+    # import pdb; pdb.set_trace()
