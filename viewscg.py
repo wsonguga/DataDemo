dc1c85b2db6df46f5aff66007d47175940ebdd73
WenZhan Song <wsong@uga.edu>
2021-06-01 09:40:13 -0400

    updated view func

--- viewscg.py
+++ viewscg.py
@@ -31,6 +31,60 @@ def onclick(event):
         pause = not pause
         # print(pause)
 
+def on_close(event):
+    print('Closed Figure!')
+    quit()
+
+def plot_data_set(data ,labels):
+    N = data.shape[0]
+    # N = 20 # data_set.shape[0]
+    R = 10 #10 # num of rows
+    C = 2 # num of cols
+
+    fig, ax = plt.subplots()
+    # ax.plot(np.random.rand(10))
+
+    k = 0
+    total = 0
+    while(True): 
+        if(total >= N):
+            break
+        fig.canvas.mpl_connect('button_press_event', onclick)
+        fig.canvas.mpl_connect('close_event', on_close)
+
+            # plt.text(0, 0, "Single click to pause/unpause, double click to quit")
+        fig.text(0.3, 0.95, "Single click to pause/unpause, double click to quit. Do not click on window close icon", size=10, rotation=0,
+            ha="center", va="center",
+            bbox=dict(boxstyle="round",
+                    ec=(1., 0.5, 0.5),
+                    fc=(1., 0.8, 0.8),
+                    )
+            )
+        for col in range(C):
+            if(total >= N):
+                    break
+            for ind in range(R):
+                if(total >= N):
+                    break
+                index_row = k*C*R + col*R+ ind
+                if labels[index_row][-1] == 1:
+                    color = 'k-' # bad data
+                else:
+                    color = 'g-' # good data
+                plt.subplot(R,2,2*ind+col+1)
+                plt.cla()   # clear previous plot
+                plt.plot(data[index_row], color)
+                plt.title(f'index: [{index_row}] labels: {labels[index_row].astype(int)}')
+                plt.ylabel('Amplitude')
+                plt.xlabel('Number of Samples')
+                total += 1
+
+        k += 1
+        plt.pause(0.05)
+        while(pause):
+            plt.pause(0.05)
+
+    plt.show()
 
 if(len(sys.argv) > 1):
     file = sys.argv[1] #http
@@ -53,35 +107,4 @@ for file in file_list:
     data = data_set[:,0:1000]
     labels = data_set[:,1000:]
 
-    fig, ax = plt.subplots()
-    # ax.plot(np.random.rand(10))
-
-    # index_col = []
-    for k in range(int(N/(2*M))): 
-        cid = fig.canvas.mpl_connect('button_press_event', onclick)
-            # plt.text(0, 0, "Single click to pause/unpause, double click to quit")
-        fig.text(0.3, 0.95, "Single click to pause/unpause, double click to quit. Do not click on window close icon", size=10, rotation=0,
-            ha="center", va="center",
-            bbox=dict(boxstyle="round",
-                    ec=(1., 0.5, 0.5),
-                    fc=(1., 0.8, 0.8),
-                    )
-            )
-        for col in [1, 2]:
-            for ind in range(M):
-                index_row = k*2*M + (col-1)*M+ ind
-                if labels[index_row][-1] == 1:
-                    color = 'k-' # bad data
-                else:
-                    color = 'g-' # good data
-                plt.subplot(M,2,2*ind+col)
-                plt.cla()   # clear previous plot
-                plt.plot(data[index_row], color)
-                plt.title(f'index: [{index_row}] labels: {labels[index_row].astype(int)}')
-                plt.ylabel('Amplitude')
-                plt.xlabel('Number of Samples')
-        plt.pause(0.05)
-        while(pause):
-            plt.pause(0.05)
-
-    plt.show()
+    plot_data_set(data, labels)
