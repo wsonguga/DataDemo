9445a29e6447689b52a8cba3a9299dce8e811608
WenZhan Song <wsong@uga.edu>
2022-02-27 16:55:23 -0500

    make simscg.py flexible with argv

--- simscg.py
+++ simscg.py
@@ -14,83 +14,38 @@ import matplotlib.pyplot as plt
 import random
 from scipy import signal
 from tqdm import tqdm
-#%matplotlib inline
 
-plt.rcParams['figure.figsize'] = [20, 10]  # Bigger images
+if __name__ == '__main__':
 
-# gen_list = [
-#             ['training_set',5000, 0.15], #5000
-#             ['good_set',500, 0.15], #1500
-#             ['bad_set',500, 0.75], #1500
-#             ['mixed_set',500, -1],
-#             ]
+    if(len(sys.argv) > 2):
+        data_file = sys.argv[1] #htt
+        N = int(sys.argv[2])
+        noise = float(sys.argv[3])
+    else:
+        print(f"Usage: {sys.argv[0]} path_file num_rows noise_level \n where noise level (amplitude of the laplace noise).")
+        print(f"Example: {sys.argv[0]} ../../data/simu.1000_6.npy 10000 0.5")       
+        exit()
 
-gen_list = [
-            ['scg_synthetic_set',10, 0], #5000
-            ]
-
-for gen_info in gen_list:
-    # import pdb; pdb.set_trace()
-    file_name = gen_info[0]
-    N = int(gen_info[1])
-
-    duration = 10
+    fs = 100
+    duration = 10 # 10 seconds
     simulated_data = []
 
-    # plt.figure(figsize=(20, 4))
-
     for ind in tqdm(range(N)):
-        heart_rate = random.randint(60, 90)
-        respiratory_rate = random.randint(10, 25)
-        systolic = random.randint(90,150)
-        diastolic = random.randint(60,90) #+ systolic
-        while (systolic - diastolic > 60 or systolic - diastolic < 20):
-            diastolic = random.randint(60,90)
-        fs = 100
+        heart_rate = random.randint(50, 150)
+        respiratory_rate = random.randint(10, 30)
+        systolic = random.randint(90, 180)
+        diastolic = systolic - random.randint(40,50) #+ systolic
+        # while (systolic - diastolic > 60 or systolic - diastolic < 20):
+        #     diastolic = random.randint(60,90)
         print('hr:', heart_rate, 'rr:', respiratory_rate, 
               'sp:', systolic, 'dp:', diastolic)
-        noise = float(gen_info[2])
-        if noise == -1:
-            noise = random.choice([0.15, 0.5, 0.75])
-            # print (noise)
-
+       
         data = nk.scg_simulate(duration=duration, sampling_rate=fs, noise=noise, heart_rate=heart_rate, respiratory_rate=respiratory_rate, systolic=systolic, diastolic=diastolic)
         ## N + 6 size. 6 are [mat_int(here 0 for synthetic data), time_stamp, hr, rr, sbp, dbp]
         simulated_data.append(list(data)+[0]+[ind]+[heart_rate]+[respiratory_rate]+[systolic]+[diastolic])
 
-        # plot the signals and spectrogram
-        x = data #/np.linalg.norm(data)
-        fmin = 0
-        fmax = 10
-        npseg = 200
-        slidingstep=20
-
-        # import pdb; pdb.set_trace()
-
-
-        # plt.subplot(N,2,2*ind+1)
-
-        # plt.subplot(N,1,ind+1)
-
-        # plt.plot(x)
-        # plt.title(f'Raw, HR {heart_rate} SBP {systolic} DBP {diastolic} Noise {noise}')
-        # plt.ylabel('Amplitude')
-        # plt.xlabel('Number of Samples')
-
-        # plot spectrogram
-        # f, t, Zxx = signal.stft(x, fs, nperseg=npseg, noverlap = npseg-slidingstep)
-        # f, t, Zxx = signal.spectrogram(x, fs)
-
-        # plt.subplot(N,2,2*ind+2)
-        # plt.pcolormesh(t, f[int(fmin*npseg/fs):int(fmax*npseg/fs)+1], np.abs(Zxx[int(fmin*npseg/fs):int(fmax*npseg/fs)+1,:]), shading='gouraud')
-        # #plt.pcolormesh(t, f, np.abs(Zxx), shading='gouraud')
-        # plt.title(f'STFT, HR {heart_rate} SBP {systolic} DBP {diastolic}')
-        # plt.ylabel('Frequency [Hz]')
-        # plt.xlabel('Time [sec]')
-        ind +=1
-
     # plt.show()
     simulated_data = np.asarray(simulated_data)
-    np.save(f'./data/{file_name}',simulated_data)
-    print(f'{file_name} is generated and saved!')
+    np.save(data_file,simulated_data)
+    print(f'{data_file} is generated and saved!')
     # import pdb; pdb.set_trace()
